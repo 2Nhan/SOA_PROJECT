248d7424ec4edba158c4052103698cac7214bb61
2Nhan <letunhan137@gmail.com>
2026-04-25 23:42:51 +0700

    commit

--- microservices/supplier/index.js
+++ microservices/supplier/index.js
@@ -34,6 +34,7 @@ app.use(session({
 
 app.use((req, res, next) => {
   res.locals.currentUser = req.session.user || null;
+  res.locals.shopServiceUrl = process.env.SHOP_SERVICE_URL || "http://localhost:8080";
   next();
 });
 
